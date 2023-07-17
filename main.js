1eb11b98551761eb4ad81990d42e004fd3099254
JaBoe97 <boehler.jana@gmail.com>
2023-07-17 20:06:04 +0200

    thumbnail hover, thumbnail active border, sum

--- main.js
+++ main.js
@@ -11,6 +11,9 @@ function plusDivs(n) {
 
 function currentDiv(n) {
   showDivs((slideIndex = n));
+  /* thumbnail active border */
+  $(".thumbnail-active").removeClass("thumbnail-active");
+  $("#thumbnail-container").children()[n - 1].classList.add("thumbnail-active");
 }
 
 function showDivs(n) {
@@ -28,14 +31,6 @@ function showDivs(n) {
 
   x[slideIndex - 1].style.display = "block";
 }
-
-/* menu toggle */
-/* const toggleButton = document.getElementById("menuToggleButton");
-const navList = document.getElementById("navList");
-
-toggleButton.addEventListener("click", () => {
-  navList.classList.toggle("active");
-}); */
 
 /* counter */
 let counter = 0;
@@ -57,7 +52,7 @@ function decrementCounter() {
 
 /* cart Button */
 function toggleCart() {
-  $("#cart-window").toggleClass("active");
+  $("#cart-window").toggleClass("displayed");
 }
 
 /* delete cart items */
@@ -72,10 +67,10 @@ function deleteCart() {
 
 /* Add to cart button */
 function addToCart() {
-  $("#cart-window").addClass("active");
   $("#cart-div").remove();
   $("#cart-counter-bubble").remove();
   if (counter > 0) {
+    $("#cart-window").addClass("displayed");
     $("#empty-cart-placeholder").css("display", "none");
     $("#checkout-button").addClass("checkout-button-active");
     const cartImage =
@@ -85,10 +80,10 @@ function addToCart() {
       counter +
       '<span class="cart-counter">' +
       "&nbsp; &nbsp;" +
-      "$" +
+      '<span class="sum">$' +
       counter * 125 +
-      "</span>" +
-      "</div>";
+      ".00";
+    "</span>" + "</span>" + "</div>";
     const cartDelete =
       '<button class="delete-button" onclick="deleteCart()"><svg width="14" height="16" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> <defs> <path d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z" id="a"/> </defs> <use fill="#C3CAD9" fill-rule="nonzero" xlink:href="#a" /></svg></button>';
     $("#checkout-button").before('<div id="cart-div" class="cart-div"></div>');
